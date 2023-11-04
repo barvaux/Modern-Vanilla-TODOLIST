9debc4afaf7051c94167db77cdbb40b0880632b0
LoremIpsum <k.barvaux.kevin@gmail.com>
2023-11-04 22:44:01 +0100

    Rectification du code et de l'edit. Code fonctionnel

--- js/DB.js
+++ js/DB.js
@@ -1,25 +1,35 @@
 export default class DB {
+    // Méthode pour définir l'URL de l'API
     static setApiURL(data) {
+      // Stocker l'URL de l'API fournie en tant que propriété statique
       this.apiURL = data;
     }
   
+    // Méthode pour récupérer tous les todos depuis l'API
     static async findAll() {
+      // Effectuer une requête asynchrone pour obtenir la liste de todos depuis l'URL de l'API
       const response = await fetch(this.apiURL + "/todos");
+      // Parser la réponse JSON et la retourner
       return await response.json();
     }
-
+  
+    // Méthode pour ajouter un nouveau todo à l'API
     static async addOne(data) {
-      const response = await fetch(this.apiURL + "/todos",{
-        method : 'post',
-        headers:{
-          "Content-Type":"application/json"
+      // Effectuer une requête asynchrone pour ajouter un todo à l'API
+      const response = await fetch(this.apiURL + "/todos", {
+        method: 'post',
+        headers: {
+          "Content-Type": "application/json"
         },
         body: JSON.stringify(data)
       });
+      // Parser la réponse JSON et la retourner
       return await response.json();
     }
-
+  
+    // Méthode pour mettre à jour un todo existant dans l'API
     static async update(id, data) {
+      // Effectuer une requête asynchrone pour mettre à jour un todo spécifique dans l'API
       const response = await fetch(`${this.apiURL}/todos/${id}`, {
         method: 'put',
         headers: {
@@ -27,28 +37,35 @@ export default class DB {
         },
         body: JSON.stringify(data),
       });
-    
+  
+      // Vérifier si la réponse est OK (réussie) ou gérer l'erreur
       if (response.ok) {
+        // Parser la réponse JSON et la retourner
         return await response.json();
       } else {
+        // Lancer une erreur en cas d'échec de la mise à jour
         throw new Error('Une erreur est survenue lors de la mise à jour');
       }
     }
-    
+  
+    // Méthode pour supprimer un todo spécifique de l'API
     static async delete(id) {
       try {
+        // Effectuer une requête asynchrone pour supprimer un todo spécifique de l'API
         const response = await fetch(`${this.apiURL}/todos/${id}`, {
           method: 'DELETE',
         });
+  
+        // Vérifier si la réponse est OK (réussie) ou gérer l'erreur
         if (!response.ok) {
+          // Lancer une erreur en cas d'échec de la suppression
           throw new Error(`Erreur HTTP ! statut : ${response.status}`);
         }
+        // Parser la réponse JSON et la retourner
         return await response.json();
       } catch (error) {
         console.error("Une erreur est survenue lors de la suppression :", error);
       }
     }
-    
-    
-
   }
+  
--- js/components/todo/Todo.js
+++ js/components/todo/Todo.js
@@ -1,10 +1,12 @@
 import getTemplate from './template.js';
 import './styles.scss';
-import DB from "../../DB.js";
+import DB from '../../DB.js';
 
+// Définition de la classe Todo
 export default class Todo {
   // Constructeur de la classe Todo
   constructor(data) {
+    // Initialisation des propriétés du todo à partir des données fournies
     this.id = data.id;
     this.content = data.content;
     this.completed = data.completed;
@@ -13,64 +15,69 @@ export default class Todo {
 
   // Méthode pour afficher un todo
   render(el) {
+    // Insère le contenu du template dans l'élément HTML fourni
     el.insertAdjacentHTML('beforeend', getTemplate(this));
     const newTodo = el.lastElementChild;
 
-    // Ajout d'un écouteur d'événement pour le changement d'état du todo
+    // Ajoute des gestionnaires d'événements pour le changement d'état, la suppression et l'édition du todo
     const checkbox = newTodo.querySelector('.toggle');
     if (checkbox) {
       checkbox.addEventListener('change', this.toggleCompleted.bind(this));
     }
-    
-    // Ajout d'un écouteur d'événement pour la suppression du todo
+
     const deleteButton = newTodo.querySelector('.destroy');
     if (deleteButton) {
       deleteButton.addEventListener('click', this.deleteTodo.bind(this));
     }
-    
-    // Ajout d'un écouteur d'événement pour l'édition du contenu du todo
+
     const label = newTodo.querySelector('.content');
     if (label) {
       label.addEventListener('dblclick', this.editTodo.bind(this));
     }
   }
 
-  // Méthode pour changer l'état d'un todo
+  // Méthode pour changer l'état d'une todo
   async toggleCompleted(e) {
+    // Met à jour l'état d'une todo en fonction de la case à cocher
     this.completed = e.target.checked;
     const todoElement = e.target.closest('li');
 
     try {
+      // Met à jour l'état d'une todo dans la base de données
       await DB.update(this.id, { completed: this.completed });
 
+      // Met à jour les styles en conséquence
       if (this.completed) {
         todoElement.classList.add('completed');
       } else {
         todoElement.classList.remove('completed');
       }
     } catch (error) {
-      console.error('Failed to update todo', error);
+      console.error('Échec de la mise à jour du todo', error);
     }
     this.updateCount();
   }
 
-  // Méthode pour supprimer un todo
+  // Méthode pour supprimer une todo
   async deleteTodo(e) {
+    // Supprime le todo de la base de données
     await DB.delete(this.id);
     const todoElement = e.target.closest('li');
     if (todoElement) {
-        todoElement.remove();
+      todoElement.remove();
     }
 
+    // Supprime également le todo de la liste des todos
     const index = this.todoListInstance.todos.indexOf(this);
     if (index > -1) {
-        this.todoListInstance.todos.splice(index, 1);
+      this.todoListInstance.todos.splice(index, 1);
     }
 
+    // Met à jour le compteur
     this.updateCount();
   }
 
-  // Méthode pour mettre à jour le contenu d'un todo
+  // Méthode pour mettre à jour le contenu d'une todo
   async updateContent(e) {
     this.updateTask = e.target.content;
     const todoElement = e.target.closest('li');
@@ -78,19 +85,19 @@ export default class Todo {
     try {
       await DB.update(this.id, { content: this.content });
     } catch (error) {
-      console.error('Failed to update todo', error);
+      console.error('Échec de la mise à jour du todo', error);
     }
     this.updateCount();
   }
 
-  // Méthode pour éditer un todo
+  // Méthode pour éditer une todo
   editTodo(e) {
     const label = e.target;
     const listItem = label.closest('li');
     const input = document.createElement('input');
     input.type = 'text';
     input.value = this.content;
-    input.className = 'edit'; 
+    input.className = 'edit';
     input.addEventListener('blur', this.updateTodo.bind(this));
     input.addEventListener('keyup', (e) => {
       if (e.code === 'Enter' && input.value.trim() !== '') {
@@ -100,9 +107,9 @@ export default class Todo {
     listItem.appendChild(input);
     listItem.classList.add('editing');
     input.focus();
-}
+  }
 
-  // Méthode pour mettre à jour un todo après édition
+  // Méthode pour mettre à jour une todo après l'edit
   async updateTodo(e) {
     const input = e.target;
     const newContent = input.value.trim();
@@ -116,15 +123,13 @@ export default class Todo {
       await DB.update(this.id, { content: newContent });
       this.content = newContent;
     } catch (error) {
-      console.error('Failed to update todo', error);
+      console.error('Échec de la mise à jour du todo', error);
     }
-}
+  }
 
-updateCount() {
-  const event = new Event('updateCounter');
-  document.dispatchEvent(event);
-}
-
-
-
+  // Méthode pour mettre à jour le compteur
+  updateCount() {
+    const event = new Event('updateCounter');
+    document.dispatchEvent(event);
+  }
 }
